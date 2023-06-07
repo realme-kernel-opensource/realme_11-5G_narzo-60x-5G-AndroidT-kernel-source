132c5c27b81a795fbac47194bba2e676dbf2a22b
Duo Yu <duo.yu@mediatek.com>
2023-06-07 16:44:02 +0800

    [ALPS07797054]  pq: add lock for check trigger
    add lock for check trigger to avoid KE
    
    MTK-Commit-Id: cc27efb9a623071cdaf760471a906940774c3fce
    
    Signed-off-by: Duo Yu <duo.yu@mediatek.com>
    CR-Id: ALPS07797054
    Feature: display_pq
    Change-Id: I5257cc7c84be6f2109b7e45dc4b238e70f6c9d1b
    (cherry picked from commit de1fc0e02a4bcc62dbfddc9e526cbae53376b050)

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_c3d.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_c3d.c
@@ -724,7 +724,7 @@ int mtk_drm_ioctl_c3d_set_lut(struct drm_device *dev, void *data,
 
 	ret = mtk_crtc_user_cmd(crtc, comp, SET_C3DLUT, data);
 
-	mtk_crtc_check_trigger(comp->mtk_crtc, true, false);
+	mtk_crtc_check_trigger(comp->mtk_crtc, true, true);
 
 	return ret;
 }
@@ -741,7 +741,7 @@ int mtk_drm_ioctl_bypass_c3d(struct drm_device *dev, void *data,
 
 	ret = mtk_crtc_user_cmd(crtc, comp, BYPASS_C3D, data);
 
-	mtk_crtc_check_trigger(comp->mtk_crtc, true, false);
+	mtk_crtc_check_trigger(comp->mtk_crtc, true, true);
 	return ret;
 }
 
